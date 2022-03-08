9bf2be5c386d1cd68460812a44979ad2d63a72ad
Lê Tuấn Anh <82755636+03-01-0v0@users.noreply.github.com>
2022-03-08 16:34:31 +0700

    Delete week3 directory

--- week3/client/pages/Components/account.js
+++
@@ -1,8 +0,0 @@
-import styles from '../../styles/account.module.css'
-export default function Account() {
-    return (
-        <div className={styles.account}>
-        <p>Welcome John<i className="material-icons">account_circle</i></p>
-        </div>
-    )
-}
